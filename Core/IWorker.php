64bf66f034b149efcb385a93bee0fb68643273f3
Shane Harter <shane.harter@gmail.com>
2012-06-06 10:08:57 -0700

    Add check_environment back to the IWorker interface

--- Core/IWorker.php
+++ Core/IWorker.php
@@ -33,4 +33,11 @@ interface Core_IWorker
 	 * @return void
 	 */
 	public function teardown();
+
+    /**
+     * This is called during object construction to validate any dependencies
+     * @return Array    Return array of error messages (Think stuff like "GD Library Extension Required" or
+     *                  "Cannot open /tmp for Writing") or an empty array
+     */
+    public function check_environment();
 }
