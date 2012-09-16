fc5bbc6627f5f72d28a820ef6df1c4e033a74dbd
shaneharter <shane.harter@gmail.com>
2012-09-16 12:46:07 -0700

    Update Core/ITask.php

--- Core/ITask.php
+++ Core/ITask.php
@@ -1,12 +1,15 @@
 <?php
 
 /**
- * Objects that implement the Core_IChildInterface can be passed directly to the Core_Daemon::fork() method. This gives
- * you a way to run setup() and teardown() code specific to the code you want to run in that fork().
+ * Objects that implement Core_ITask can be passed directly to the Core_Daemon::task() method. Simple tasks can be implemented as 
+ * a closure. But more complex tasks (or those that benefit from their own setup() and teardown() code) can be more cleanly
+ * written as a Core_ITask object. 
  *
- * You can use the Core_Daemon::on(ON_FORK) method to provide universal setup code that is run after every fork and
- * in every worker. The setup() method defined here can be used if you want specific setup code run in this forked process.
- *
+ * The setup() method in a Core_ITask object is run in the newly-forked process created specifically for this task. You can 
+ * also create a Core_Daemon::on(ON_FORK) event handler that can run any setup/auditing/tracing/etc code in the parent process. 
+ * 
+ * Note: An ON_FORK event will be dispatched every time a new task() is created -- whether that is explicitely by you, or implicitely 
+ * by the Worker API. 
  */
 interface Core_ITask
 {
