180dae184949456ccfd25a5a7df7347ad29d356e
Shane Harter <shane.harter@gmail.com>
2012-11-12 12:21:19 -0800

    Untested but basically complete Socket Server plugin.

--- Core/Lib/Command.php
+++ Core/Lib/Command.php
@@ -14,12 +14,37 @@ class Core_Lib_Command
 
     public $callable;
 
-    public function match($input, Array $args = array()) {
-        $matches = null;
-        if (preg_match($this->regex, $input, $matches) == 1)
-            return call_user_func_array($this->callable, array_shift($args, $matches));
+    public $result_input;
 
-        return null;
+    public $result;
+
+    /**
+     * When true, indicate to the calling code that $input shouldn't be tested against any additional commands. Will guarantee
+     * 0 or 1 commands will match any $input.
+     * @var bool
+     */
+    public $exclusive = true;
+
+    /**
+     * Apply the $input against the command regex. If it matches, the contained $callable will be called and its result
+     * will be available in Core_Lib_Command::result. It will return true. The result will be available until match() is called again.
+     * The input matched against is also available as Core_Lib_Command::result_input
+     *
+     * The passed array of $args is passed untouched to the $callable.
+     *
+     * @param $input
+     * @param array $args
+     * @return bool
+     */
+    public function match($input, Array $args = array()) {
+        $this->result_input = $this->result = $matches = null;
+        if (preg_match($this->regex, $input, $matches) == 1) {
+            $this->result_input = $input;
+            $this->result = call_user_func_array($this->callable, array_shift($args, $matches));
+            return true;
+        }
+
+        return false;
     }
 
 }
--- Core/Plugin/Server.php
+++ Core/Plugin/Server.php
@@ -3,7 +3,13 @@
 
 class Core_Plugin_Server implements Core_IPlugin
 {
+    const COMMAND_CONNECT       = 'CLIENT_CONNCT';
+    const COMMAND_DISCONNECT    = 'CLIENT_DISCONNECT';
+    const COMMAND_DESTRUCT      = 'SERVER_DISCONNECT';
 
+    /**
+     * @var Core_Daemon
+     */
     public $daemon;
 
     /**
@@ -24,10 +30,22 @@ class Core_Plugin_Server implements Core_IPlugin
      */
     public $socket;
 
+    /**
+     * Maximum number of concurrent clients
+     * @var int
+     */
     public $max_clients = 10;
 
+    /**
+     * Maximum bytes read from a given client connection at a time
+     * @var int
+     */
     public $max_read = 1024;
 
+    /**
+     * Array of stdClass client structs.
+     * @var stdClass[]
+     */
     public $clients = array();
 
     /**
@@ -39,6 +57,18 @@ class Core_Plugin_Server implements Core_IPlugin
     public $blocking = true;
 
     /**
+     * Write verbose logging to the application log when true.
+     * @var bool
+     */
+    public $debug = true;
+
+    /**
+     * Array of Command objects to match input against.
+     * Note: In addition to input rec'd from the client, the server will emit the following commands when appropriate:
+     * CLIENT_CONNECT(stdClass Client)
+     * CLIENT_DISCONNECT(stdClass Client)
+     * SERVER_DISCONNECT
+     *
      * @var Core_Lib_Command[]
      */
     private $commands = array();
@@ -60,6 +90,8 @@ class Core_Plugin_Server implements Core_IPlugin
         $this->socket = socket_create(AF_INET, SOCK_STREAM, 0);
         if (!socket_bind($this->socket, $this->ip, $this->port))
             throw new Exception('Could not bind to address');
+
+        socket_listen($this->socket);
     }
 
     /**
@@ -83,8 +115,8 @@ class Core_Plugin_Server implements Core_IPlugin
         $this->commands[] = $command;
     }
 
-    public function start() {
-        socket_listen($this->socket);
+    public function run() {
+
 
         $read[0] = $this->socket;
         foreach($this->clients as $client)
@@ -103,38 +135,74 @@ class Core_Plugin_Server implements Core_IPlugin
                 $this->log('Nothing waiting to be polled');
         }
 
-
+        // If the master socket is in the read array, there's a pending connection
         if (in_array($this->socket, $read))
             $this->connect();
 
 
-//            for($i = 0; $i < $this->max_clients; $i++)
-//            {
-//                if(empty($this->clients[$i]))
-//                {
-//                    $temp_sock = $this->master_socket;
-//                    $this->clients[$i] = new SocketServerClient($this->master_socket,$i);
-//                    $this->trigger_hooks("CONNECT",$this->clients[$i],"");
-//                    break;
-//                }
-//                elseif($i == ($this->max_clients-1))
-//                {
-//                    SocketServer::debug("Too many clients...   ");
-//                }
-//            }
-    }
+        foreach($this->clients as $slot => $client) {
+            if (!in_array($client->socket, $read))
+                continue;
 
+            $input = socket_read($client->socket, $this->max_read);
+            if ($input === null) {
+                $this->disconnect($slot);
+                continue;
+            }
 
-    private function connect() {
-        $client = new stdClass();
-    }
-
-    private function slot() {
-        for($i=0; $i < $this->max_clients; $i++ ) {
-
+            $this->command($input);
         }
     }
 
+    private function connect() {
+        $slot = $this->slot();
+        if ($slot === null)
+            throw new Exception(sprintf('%s::%s Failed - Maximum number of connections has been reached.', __CLASS__, __METHOD__));
+
+        $client = new stdClass();
+        $client->socket = socket_accept($this->socket);
+        if (empty($client->socket))
+            throw new Exception(sprintf('%s::%s Failed - socket_accept failed with error: %s', __CLASS__, __METHOD__, socket_last_error()));
+
+        socket_getpeername($client->socket, $client->ip);
+
+        $client->write = function($string, $term = "\r\n") use($client) {
+            if($term)
+                $string .= $term;
+
+            return socket_write($client->socket, $string, strlen($string));
+        };
+
+        $this->clients[$slot] = $client;
+        $this->command(self::COMMAND_CONNECT, array($client));
+    }
+
+    private function command($input, Array $args = array()) {
+        foreach($this->commands as $command)
+            if($command->match($input, $args) && $command->exclusive)
+                break;
+    }
+
+    private function disconnect($slot) {
+        $this->command(self::COMMAND_DISCONNECT, array($this->clients[$slot]));
+        @ socket_close($this->clients[$slot]->socket);
+        unset($this->clients[$slot]);
+    }
+
+    private function slot() {
+        for($i=0; $i < $this->max_clients; $i++ )
+            if (empty($this->clients[$i]))
+                return $i;
+
+        return null;
+    }
+
+    private function debug($message) {
+        if (!$this->debug)
+            return;
+
+        $this->daemon->log($message, 'SocketServer');
+    }
 
     private function error($message) {
         $this->daemon->error($message, 'SocketServer');
@@ -144,294 +212,3 @@ class Core_Plugin_Server implements Core_IPlugin
         $this->daemon->log($message, 'SocketServer');
     }
 }
-
-
-
-
-
-	class SocketServer
-  {
-      /*!	@var		config
-        @abstract	Array - an array of configuration information used by the server.
-       */
-      protected $config;
-
-      /*!	@var		hooks
-        @abstract	Array - a dictionary of hooks and the callbacks attached to them.
-       */
-      protected $hooks;
-
-      /*!	@var		master_socket
-        @abstract	resource - The master socket used by the server.
-       */
-      protected $master_socket;
-
-      /*!	@var		max_clients
-        @abstract	unsigned int - The maximum number of clients allowed to connect.
-       */
-      public $max_clients = 10;
-
-      /*!	@var		max_read
-        @abstract	unsigned int - The maximum number of bytes to read from a socket at a single time.
-       */
-      public $max_read = 1024;
-
-      /*!	@var		clients
-        @abstract	Array - an array of connected clients.
-       */
-      public $clients;
-
-      /*!	@function	__construct
-        @abstract	Creates the socket and starts listening to it.
-        @param		string	- IP Address to bind to, NULL for default.
-        @param		int	- Port to bind to
-        @result		void
-       */
-      public function __construct($bind_ip,$port)
-      {
-          set_time_limit(0);
-          $this->hooks = array();
-
-          $this->config["ip"] = $bind_ip;
-          $this->config["port"] = $port;
-
-          $this->master_socket = socket_create(AF_INET, SOCK_STREAM, 0);
-          socket_bind($this->master_socket,$this->config["ip"],$this->config["port"]) or die("Issue Binding");
-          socket_getsockname($this->master_socket,$bind_ip,$port);
-          socket_listen($this->master_socket);
-          SocketServer::debug("Listenting for connections on {$bind_ip}:{$port}");
-      }
-
-      /*!	@function	loop_once
-        @abstract	Runs the class's actions once.
-        @discussion	Should only be used if you want to run additional checks during server operation.  Otherwise, use infinite_loop()
-        @param		void
-        @see		infinite_loop
-        @result 	bool	- True
-      */
-      public function loop_once()
-      {
-          // Setup Clients Listen Socket For Reading
-          $read[0] = $this->master_socket;
-          for($i = 0; $i < $this->max_clients; $i++)
-          {
-              if(isset($this->clients[$i]))
-              {
-                  $read[$i + 1] = $this->clients[$i]->socket;
-              }
-          }
-
-          if(socket_select($read, $write = null, $except = null, null) < 1)
-          {
-              //	SocketServer::debug("Problem blocking socket_select?");
-              return true;
-          }
-
-          // Handle new Connections
-          if(in_array($this->master_socket, $read))
-          {
-              for($i = 0; $i < $this->max_clients; $i++)
-              {
-                  if(empty($this->clients[$i]))
-                  {
-                      $temp_sock = $this->master_socket;
-                      $this->clients[$i] = new SocketServerClient($this->master_socket,$i);
-                      $this->trigger_hooks("CONNECT",$this->clients[$i],"");
-                      break;
-                  }
-                  elseif($i == ($this->max_clients-1))
-                  {
-                      SocketServer::debug("Too many clients...   ");
-                  }
-              }
-
-          }
-
-          // Handle Input
-          for($i = 0; $i < $this->max_clients; $i++) // for each client
-          {
-              if(isset($this->clients[$i]))
-              {
-                  if(in_array($this->clients[$i]->socket, $read))
-                  {
-                      $input = socket_read($this->clients[$i]->socket, $this->max_read);
-                      if($input == null)
-                      {
-                          $this->disconnect($i);
-                      }
-                      else
-                      {
-                          SocketServer::debug("{$i}@{$this->clients[$i]->ip} --> {$input}");
-                          $this->trigger_hooks("INPUT",$this->clients[$i],$input);
-                      }
-                  }
-              }
-          }
-          return true;
-      }
-
-      /*!	@function	disconnect
-        @abstract	Disconnects a client from the server.
-        @param		int	- Index of the client to disconnect.
-        @param		string	- Message to send to the hooks
-        @result		void
-      */
-      public function disconnect($client_index,$message = "")
-      {
-          $i = $client_index;
-          SocketServer::debug("Client {$i} from {$this->clients[$i]->ip} Disconnecting");
-          $this->trigger_hooks("DISCONNECT",$this->clients[$i],$message);
-          $this->clients[$i]->destroy();
-          unset($this->clients[$i]);
-      }
-
-      /*!	@function	trigger_hooks
-        @abstract	Triggers Hooks for a certain command.
-        @param		string	- Command who's hooks you want to trigger.
-        @param		object	- The client who activated this command.
-        @param		string	- The input from the client, or a message to be sent to the hooks.
-        @result		void
-      */
-      public function trigger_hooks($command,&$client,$input)
-      {
-          if(isset($this->hooks[$command]))
-          {
-              foreach($this->hooks[$command] as $function)
-              {
-                  SocketServer::debug("Triggering Hook '{$function}' for '{$command}'");
-                  $continue = call_user_func($function,&$this,&$client,$input);
-                  if($continue === FALSE) { break; }
-              }
-          }
-      }
-
-      /*!	@function	infinite_loop
-        @abstract	Runs the server code until the server is shut down.
-        @see		loop_once
-        @param		void
-        @result		void
-      */
-      public function infinite_loop()
-      {
-          $test = true;
-          do
-          {
-              $test = $this->loop_once();
-          }
-          while($test);
-      }
-
-      /*!	@function	debug
-        @static
-        @abstract	Outputs Text directly.
-        @discussion	Yeah, should probably make a way to turn this off.
-        @param		string	- Text to Output
-        @result		void
-      */
-      public static function debug($text)
-      {
-          echo("{$text}\r\n");
-      }
-
-      /*!	@function	socket_write_smart
-        @static
-        @abstract	Writes data to the socket, including the length of the data, and ends it with a CRLF unless specified.
-        @discussion	It is perfectly valid for socket_write_smart to return zero which means no bytes have been written. Be sure to use the === operator to check for FALSE in case of an error.
-        @param		resource- Socket Instance
-        @param		string	- Data to write to the socket.
-        @param		string	- Data to end the line with.  Specify a "" if you don't want a line end sent.
-        @result		mixed	- Returns the number of bytes successfully written to the socket or FALSE on failure. The error code can be retrieved with socket_last_error(). This code may be passed to socket_strerror() to get a textual explanation of the error.
-      */
-      public static function socket_write_smart(&$sock,$string,$crlf = "\r\n")
-      {
-          SocketServer::debug("< -- {$string}");
-          if($crlf) { $string = "{$string}{$crlf}"; }
-          return socket_write($sock,$string,strlen($string));
-      }
-
-      /*!	@function	__get
-        @abstract	Magic Method used for allowing the reading of protected variables.
-        @discussion	You never need to use this method, simply calling $server->variable works because of this method's existence.
-        @param		string	- Variable to retrieve
-        @result		mixed	- Returns the reference to the variable called.
-      */
-      function &__get($name)
-      {
-          return $this->{$name};
-      }
-  }
-
-	/*!	@class		SocketServerClient
-		@author		Navarr Barnier
-		@abstract	A Client Instance for use with SocketServer
-	 */
-	class SocketServerClient
-  {
-      /*!	@var		socket
-        @abstract	resource - The client's socket resource, for sending and receiving data with.
-       */
-      protected $socket;
-
-      /*!	@var		ip
-        @abstract	string - The client's IP address, as seen by the server.
-       */
-      protected $ip;
-
-      /*!	@var		hostname
-        @abstract	string - The client's hostname, as seen by the server.
-        @discussion	This variable is only set after calling lookup_hostname, as hostname lookups can take up a decent amount of time.
-        @see		lookup_hostname
-       */
-      protected $hostname;
-
-      /*!	@var		server_clients_index
-        @abstract	int - The index of this client in the SocketServer's client array.
-       */
-      protected $server_clients_index;
-
-      /*!	@function	__construct
-        @param		resource- The resource of the socket the client is connecting by, generally the master socket.
-        @param		int	- The Index in the Server's client array.
-        @result		void
-       */
-      public function __construct(&$socket,$i)
-      {
-          $this->server_clients_index = $i;
-          $this->socket = socket_accept($socket) or die("Failed to Accept");
-          SocketServer::debug("New Client Connected");
-          socket_getpeername($this->socket,$ip);
-          $this->ip = $ip;
-      }
-
-      /*!	@function	lookup_hostname
-        @abstract	Searches for the user's hostname and stores the result to hostname.
-        @see		hostname
-        @param		void
-        @result		string	- The hostname on success or the IP address on failure.
-       */
-      public function lookup_hostname()
-      {
-          $this->hostname = gethostbyaddr($this->ip);
-          return $this->hostname;
-      }
-
-      /*!	@function	destroy
-        @abstract	Closes the socket.  Thats pretty much it.
-        @param		void
-        @result		void
-       */
-      public function destroy()
-      {
-          socket_close($this->socket);
-      }
-
-      function &__get($name)
-      {
-          return $this->{$name};
-      }
-
-      function __isset($name)
-      {
-          return isset($this->{$name});
-      }
-  }
