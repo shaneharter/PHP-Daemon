f3bd19687174690a7935a34337e225c49bbbcb88
Shane Harter <shane@trulia.com>
2013-09-27 12:23:45 -0700

    Remove incomplete HashServer example app

--- Examples/HashServer/run.php
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/php
-<?php
-require_once 'config.php';
-require_once 'error_handlers.php';
-
-use Examples\HashServer;
-
-// The run() method will start the daemon loop.
-HashServer\Daemon::getInstance()->run();
