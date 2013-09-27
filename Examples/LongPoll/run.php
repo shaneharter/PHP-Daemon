6ce03f52062b0ace570f038c1b9f17d30e7027da
Shane Harter <shane@trulia.com>
2013-09-27 12:22:50 -0700

    Remove deprecated set_filename() calls from example apps

--- Examples/LongPoll/run.php
+++ Examples/LongPoll/run.php
@@ -5,8 +5,5 @@ require_once 'error_handlers.php';
 
 use Examples\LongPoll;
 
-// The daemon needs to know from which file it was executed.
-LongPoll\Poller::setFilename(__FILE__);
-
 // The run() method will start the daemon event loop.
 LongPoll\Poller::getInstance()->run();
